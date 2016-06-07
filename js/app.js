64c204799bdb8390182226837dbd09ea9ecc4037
James <jm0393@icloud.com>
2016-06-07 11:59:06 -0500

    finalizing arcade game

--- js/app.js
+++ js/app.js
@@ -1,92 +1,84 @@
-// Enemies our player must avoid
-var Character = function () {
-    this.height = 90;
-    this.width = 101;
-}
+var Tile_Width = 101;
+    Tile_Height = 77;  
 
-var Enemy = function() {
-    this.col = -2;
-    this.row = getRandomIntInclusive(2, 3);
-	this.x = 101 * this.col;
-	this.y = 90 * this.row;
-	this.speed = getRandomIntInclusive(1, 6);
-    this.path = 'images/enemies/';
-    this.image = ['enemy-copia.png', 'enemy-bug.png'];
-    this.value = Math.floor(Math.random() * ((0-3)+1) + 2);
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = this.path + this.image[this.value];
+var Character = function ()  {
+    "use strict";
+    this.reset();
 
 };
 
+Character.prototype.render = function () {
+    "use strict";
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+};
+
+var Enemy = function () {
+    "use strict";
+    Character.call(this);
+    this.sprite = 'images/enemies/enemy-bug.png';
+};
+
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-	this.x = this.x + this.speed;
-    this.y = (90 * this.row) - (90/2);
+    "use strict";
+    this.x = this.x + this.speed;
+    this.y = Tile_Height * (this.row - 0.25);
     if (this.x > 505) {
         this.reset();
     }
-    if (player.row === this.row && (player.x >= this.x && player.x <= (this.x + 101))) {
+
+    if (player.row === this.row && (player.x >= this.x && player.x <= (this.x + Tile_Width))) {
         player.reset();
         this.reset();
     }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
 };
 
 Enemy.prototype.reset = function () {
-    this.row = getRandomIntInclusive(2, 3);
-    this.x = 101 * this.col;
-    this.y = (90 * this.row) - (90/2);
-    this.speed = getRandomIntInclusive(1, 6);
-    this.path = 'images/enemies/';
-    this.image = ['enemy-copia.png', 'enemy-bug.png'];
-    this.value = Math.floor(Math.random() * ((0-3)+1) + 2);
-    this.sprite = this.path + this.image[this.value];
+    "use strict";
+    this.col = -2;
+    this.row = getRandomIntInclusive(1, 3);
+    this.x = Tile_Width * this.col;
+    this.y = Tile_Height * this.row;
+    this.speed = getRandomIntInclusive(1, 6); 
      
 };
 
+
+var allEnemies = []; 
+    for (var i = 0; i < 4; i++) {
+        allEnemies.push(new Enemy () );
+}
+
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
-var allEnemies = [];{
-	for (var i = 0; i < 4; i++)
-		allEnemies.push(new Enemy());
-}
-
-function getRandomIntInclusive(min, max) {
-    return Math.round(Math.floor(Math.random() * (max - min + 1)) + min);
-}
 
 // Now write your own player class
 // This class requires an update(), render() and
-// a handleInput() method.
-var Player = function() {
-    this.col = 2;
-    this.row = 5;
-    this.x = 101 * this.col;
-    this.y = 75 * this.row;
-	this.move = true;
+
+function Player() {
+    "use strict";
+    Character.call(this);
+    this.moveable = true;
     this.sprite = 'images/char-boy.png';
     
-};
+}
+
+Player.prototype = Object.create (Character.prototype);
+Player.prototype.constructor = Player;
 
 Player.prototype.update = function() {
+    "use strict";
     if (this.moveable) {
-    this.x = 101 * this.col;
-    this.y = 75 * this.row;
+    this.x = Tile_Width * this.col;
+    this.y = Tile_Height * this.row;
 
     }
-    if (this.y < 75 && this.moveable) {
+    if (this.y < Tile_Height && this.moveable) {
         this.moveable = false;
         return true;
     }
@@ -95,14 +87,11 @@ Player.prototype.update = function() {
 
 
 Player.prototype.reset = function() {
-  	this.col = 2;
+    "use strict";
+    this.col = 2;
     this.row = 5;
     this.moveable = true;
 
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 var player = new Player();
@@ -110,8 +99,8 @@ var player = new Player();
 // Place the player object in a variable called player
 
 Player.prototype.handleInput = function(key) {
-
-	switch (key) {
+    "use strict";
+    switch (key) {
 
         case "left" : this.col--;
         break;
@@ -124,12 +113,22 @@ Player.prototype.handleInput = function(key) {
         
         case "down" : this.row++;
         break;
+
     }
-    if (this.col < 0) this.col = 0;
-    if (this.col > 4) this.col = 4;
-    if (this.row > 5) this.row = 5;
+
+    if (this.col < 0) {
+        this.col = 0;
+    }
+    if (this.col > 4) {
+        this.col = 4;
+    }
+    if (this.row > 5) {
+        this.row = 5;
+    }
     //Player wins the game reset
-    if (this.row < 0) this.reset();
+    if (this.row < 0) {
+        this.reset();
+    }
 
 };
 
@@ -147,3 +146,7 @@ document.addEventListener('keyup', function(e) {
    
 });
 
+function getRandomIntInclusive(min, max) {
+      "use strict";
+    return Math.round(Math.floor(Math.random() * (max - min + 1)) + min);
+}
